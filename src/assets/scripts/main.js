fb1b9e1dc2c734699ad79752935aff6046170358
lali <l.osorio@bcasapp.com>
2023-04-07 14:17:27 +0200

    pagina acabada

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,71 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+;+(function () {
+  const university = 'UOC'
+  console.log(`Hello, ${university}!`)
+})()
+
+// Navbar
+
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction () {
+  if (window.scrollY > window.innerHeight) {
+    document.getElementById('navbar').style.backgroundColor = '#8d301f'
+  } else {
+    document.getElementById('navbar').style.backgroundColor = 'transparent'
+  }
+}
+
+// Carousel
+const progressBar = document.querySelector('.progress')
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+})
+
+progressBar.addEventListener('animationend', myEndFunction)
+
+// Retrigger Animation on Slide Change
+
+function myEndFunction () {
+  swiper.slideNext()
+  progressBar.style.animation = 'none'
+  void progressBar.offsetWidth // Triggers Reflow
+  progressBar.style.animation = null
+}
+
+// Reset Progress Bar On Slide Change
+
+swiper.on('slideChange', function () {
+  progressBar.style.animation = 'none'
+  void progressBar.offsetWidth // Triggers Reflow
+  progressBar.style.animation = null
+  progressBar.style.animationPlayState = 'paused' // Optional
+})
+
+// Pause Carousel/Progress Bar On Hover
+
+document.querySelectorAll('.swiper, .carousel-progress').forEach(item => {
+  item.addEventListener('mouseenter', function () {
+    progressBar.style.animationPlayState = 'paused'
+  })
+})
+
+document.querySelectorAll('.swiper, .carousel-progress').forEach(item => {
+  item.addEventListener('mouseleave', function () {
+    progressBar.style.animationPlayState = 'running'
+  })
+})
